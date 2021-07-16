17e83fcf3f4b638f203f3975762c1058724fbf2b
enock <enckkipronoh@gmail.com>
2021-07-16 14:31:46 +0300

    adding the arrays to store female akan names

--- js/script.js
+++ js/script.js
@@ -1,3 +1,3 @@
-var maleakanNames =["Kwasi","Kwendwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"];
-Var 
-var weekDays=["Sunday", "Monday","Tuesday","Wedneday","Thursday","Friday","Saturday",];
+var maleAkanNames = ["Kwasi","Kwendwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"];
+var femaleAkanNames =  ["Akosua", "Adwoa","Abenaa","Akua","Yaa","Afua","Ama"];
+var weekDays = ["Sunday", "Monday","Tuesday","Wedneday","Thursday","Friday","Saturday",];
