4a8ce6c274d6be9f61fac1608f200e02f3578f35
enock <enckkipronoh@gmail.com>
2021-07-16 14:47:35 +0300

    adding the variables for year, month and day

--- js/script.js
+++ js/script.js
@@ -1,3 +1,9 @@
 var maleAkanNames = ["Kwasi","Kwendwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"];
 var femaleAkanNames =  ["Akosua", "Adwoa","Abenaa","Akua","Yaa","Afua","Ama"];
 var weekDays = ["Sunday", "Monday","Tuesday","Wedneday","Thursday","Friday","Saturday",];
+
+function akanNaming(){
+ var year = document.getElementById("yob").value;
+ var month = document.getElementById("mob").value;
+ var day = document.getElementById("dob").value;
+}
