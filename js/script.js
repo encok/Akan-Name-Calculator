207d67367264a5b61d005a9c8b8fc69db49b7894
enock <enckkipronoh@gmail.com>
2021-07-17 15:08:55 +0300

    validating form

--- js/script.js
+++ js/script.js
@@ -1,26 +1,31 @@
-// akans main function
-function computation(){
+
+function akanNaming(){
     // arrays declaration
     let maleNames=["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
     let FemaleNames=["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
     
     // variables declaration
     
-    let fullyear=parseInt(document.getElementById('yob').value);
+    let wholeyear=parseInt(document.getElementById('yob').value);
     let month=parseInt(document.getElementById('mob').value);
     let day=parseInt(document.getElementById('dob').value);
     let gender=document.getElementById('gender').value;
 
     // slice fullyear
-    let year1=fullyear.toString().slice(-2);
+    let year1=wholeyear.toString().slice(-2);
     let year=parseInt(year1);
 
-    let century=Math.ceil((fullyear)/100)-1;
+    let century=Math.ceil((wholeyear)/100)-1;
     // computation
     let dayOfWeek=((((century/4)-2*century-1)+((5*year/4))+((26*(month+1)/10))+day)%7);
     // validation
     if(day>0&&day<32){
-        if(month>0&&month<13){
+
+       
+     
+            
+        
+        if(month>0&&month<31){
             if(gender=='Male'){
                 let i=0;
                        for(i; i<=6;i++){
@@ -42,12 +47,16 @@ function computation(){
                     }
                 }             
             }
+        
+
         }
         else{
             alert("Invalid month entered");
         }
     }
+   
+
     else{
-        alert("Invalid day entered");
+        alert("Invalid day");
     }
 }
